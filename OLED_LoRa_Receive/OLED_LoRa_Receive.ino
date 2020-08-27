b86c32ffcfab88c58ca3ee998c95722da79a46af
h0rn3t <vamp.null@gmail.com>
2020-08-27 22:28:03 +0300

    fixes

--- OLED_LoRa_Receive/OLED_LoRa_Receive.ino
+++ OLED_LoRa_Receive/OLED_LoRa_Receive.ino
@@ -1,8 +1,7 @@
 #include <SPI.h>
 #include <LoRa.h>
-#include <Wire.h>  
-#include "SSD1306.h" 
-#include "images.h"
+#include <Wire.h>
+#include "SSD1306.h"
 
 #define SCK     5    // GPIO5  -- SX1278's SCK
 #define MISO    19   // GPIO19 -- SX1278's MISO
@@ -12,59 +11,76 @@
 #define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
 #define BAND    868E6
 
-SSD1306 display(0x3c, 21, 22);
+SSD1306 display(0x3c, 4, 15);
 String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
 
 
-void loraData(){
-  display.clear();
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.setFont(ArialMT_Plain_10);
-  display.drawString(0 , 15 , "Received "+ packSize + " bytes");
-  display.drawStringMaxWidth(0 , 26 , 128, packet);
-  display.drawString(0, 0, rssi); 
-  display.display();
-  Serial.println(rssi);
+
+void loraData(String packet){
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0 , 15 , "Received "+ packSize + " bytes");
+    display.drawStringMaxWidth(0 , 26 , 128, packet);
+    display.drawString(0, 0, rssi);
+    display.display();
 }
 
+void logo(){
+    display.clear();
+    display.drawString(0, 0, "Loading...");
+    display.display();
+}
+
+void log(String message){
+    display.clear();
+    display.drawString(0, 0, message);
+    display.display();
+}
+
+
 void cbk(int packetSize) {
-  packet ="";
-  packSize = String(packetSize,DEC);
-  for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
-  rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
-  loraData();
+    log("recieved...");
+    packet ="";
+    packSize = String(packetSize,DEC);
+    for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
+    rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
+    loraData(packet);
 }
 
 void setup() {
-  pinMode(16,OUTPUT);
-  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
-  delay(50); 
-  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high、
-  
-  Serial.begin(115200);
-  while (!Serial);
-  Serial.println();
-  Serial.println("LoRa Receiver Callback");
-  SPI.begin(SCK,MISO,MOSI,SS);
-  LoRa.setPins(SS,RST,DI0);  
-  if (!LoRa.begin(868E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
-  //LoRa.onReceive(cbk);
-  LoRa.receive();
-  Serial.println("init ok");
-  display.init();
-  display.flipScreenVertically();  
-  display.setFont(ArialMT_Plain_10);
-   
-  delay(1500);
+    pinMode(16,OUTPUT);
+    digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+    delay(50);
+    digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high、
+
+    Serial.begin(9600);
+    while (!Serial);
+    Serial.println();
+    Serial.println("LoRa Receiver Callback");
+    SPI.begin(SCK,MISO,MOSI,SS);
+    LoRa.setPins(SS,RST,DI0);
+    if (!LoRa.begin(BAND)) {
+        Serial.println("Starting LoRa failed!");
+        log("Starting LoRa failed!");
+        while (1);
+    }
+    //LoRa.onReceive(cbk);
+    LoRa.receive();
+    display.init();
+    display.flipScreenVertically();
+    display.setFont(ArialMT_Plain_10);
+    log("init ok");
+    delay(1500);
+    //loop();
 }
 
 void loop() {
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) { cbk(packetSize);  }
-  delay(10);
+    int packetSize = LoRa.parsePacket();
+    if (packetSize) {
+        cbk(packetSize);
+    }
+    delay(10);
 }
